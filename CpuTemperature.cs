c1437cb552270925a3b458889b4a5819a93d7c1e
Dave Glover <dglover@microsoft.com>
2019-02-25 10:46:00 +1100

    first commit

---
+++ CpuTemperature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace RPi.Sensors.Devices.Cpu
+{
+    public class Cpu
+    {
+        public double? GetTemperature()
+        {
+            using (FileStream fileStream = new FileStream("/sys/class/thermal/thermal_zone0/temp", FileMode.Open, FileAccess.Read))
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string data = reader.ReadLine();
+                int temperature;
+                if (int.TryParse(data, out temperature))
+                {
+                    return Math.Round(temperature / 1000F, 1);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# RPi.CpuTemperature.NET.Standard
